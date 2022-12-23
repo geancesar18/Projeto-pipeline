0d439b5ce97a3da1e8bb47e22489d72a4f577095
geancesar18 <110560051+geancesar18@users.noreply.github.com>
2022-12-23 17:35:06 -0300

    Add files via upload

---
+++ kubernetes-pipeline-projeto2-dio/app/conexao.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mysql-connection";
+$username = "root";
+$password = "1234";
+$database = "meubanco";
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
